21be9b4a0cf62ba7375aa55c91ecdf38acaca5e3
Gustavo <gustavo.cunha93@gmail.com>
2025-03-18 17:13:21 -0300

    Página sudoeste ou JB?

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function schedule(mentorName) {
-    alert(`Você escolheu agendar com ${mentorName}.`);
-  }
+function showUnit(unitId) {
+  // Esconde o modal
+  document.getElementById('unitModal').style.display = 'none';
+
+  // Esconde todas as seções de unidade
+  const units = document.querySelectorAll('.unit');
+  units.forEach(unit => unit.classList.remove('active'));
+
+  // Mostra a seção selecionada
+  document.getElementById(unitId).classList.add('active');
+}
